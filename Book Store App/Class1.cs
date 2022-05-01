5cc5fcb38d0741f9974cc396370784fb2a662c56
NaumVitanov <vitanovnaum@gmai.com>
2022-05-01 12:17:26 +0300

    initial commit

---
+++ Book Store App/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace Book_Store_App
+{
+    public class Library
+    {
+        public string? Name;
+        public string? Color;
+        public int Pages;
+        public int Year;
+
+        public Library (string BookName, string BookColor, int BookPages, int BookYear)
+        {
+            Name = BookName;
+            Color = BookColor;
+            Pages = BookPages;
+            Year = BookYear;
+        }
+    }
+}
---
+++ Book Store App/Program.cs
@@ -0,0 +1,10 @@
+﻿using Book_Store_App;
+
+Library Book1 = new("Harry Potter 1" , "Red" , 256 , 1996);
+
+Library Book2 = new("Harry Potter 2","Gren" , 320 , 2002);
+
+Console.WriteLine(Book1.Name);
+Console.WriteLine(Book1.Year);
+Console.WriteLine(Book2.Pages);
+Console.WriteLine(Book2.Color);
